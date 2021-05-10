fbe84366267543ee369313dd6f4830da699b4737
nvCuong <nguyenvancuong215@gmail.com>
2021-05-11 01:01:22 +0700

    ấd

---
+++ Web-Advance/lesson2/script.js
@@ -0,0 +1,122 @@
+// 1, các kiểu dữ liệu
+//     - number: 1,2
+//     - string: "", '', ``
+//     - boolean: true/ false
+
+// 2, biến (variable)
+//     - let 
+//     - var
+//     - const
+//         let a = 6
+//         a = "Bon"
+//         console.log(a);
+
+//         const pi = 3.14
+//         pi = 90 => erorr
+
+// 3, các toán tử (operator)
+//      +: Cộng
+//      -: Trừ
+//      *: nhân
+//      /: Chia
+//      %: lấy phần dư : ví dụ: 21%2
+
+// 4, if else
+//     if(a>b){
+//         console.log("a lớn hơn b nè");
+//     }else{
+//         console.log("a k lớn b đâu");
+//     }
+
+// 5, loop ( for, while)
+
+    // for(let i = 9; i<20; i = i +3){
+    //     console.log(i);
+    // }
+
+
+    // chữa homework 
+
+// i=i+1
+// i+=1
+// i++
+
+
+
+// for(let i = 0; i<=10; i+=1){
+//     if(i%2 == 0){
+//         console.log(i);
+//     }
+
+// }
+
+let games = [
+    {
+      name:"roblox",
+      url:"https://th.bing.com/th/id/R7652d7f972676d12ea15f9386a784cf0?rik=TNFxJpcuXk6Ncg&pid=ImgRaw"
+    },
+    {
+      name:"minecraft",
+      url:"https://getdrawings.com/free-icon/minecraft-launcher-icon-69.png"
+    },
+    {
+      name:"fortnite",
+      url:"https://th.bing.com/th/id/R275c8b2e8734e0ae3e0d4f6f662aa00f?rik=Ii5%2bccmwp04hLA&pid=ImgRaw"
+    },
+    {
+      name:"among us",
+      url:"https://among-us.io/wp-content/uploads/2020/09/among-us-icon.jpg"
+    },
+    {
+      name:"valorant",
+      url:"https://external-preview.redd.it/9LAUtPMJxhqx8H2KAv-oipyMnAwFErAUyhUwsz-aNiM.jpg?auto=webp&s=68f81f665ee87fd43dce4465c69bac79dd45821f"
+    },
+    {
+      name:"rocket league",
+      url:"https://th.bing.com/th/id/Rd8ace1286a2c5e8bb335fa5f80c14ef4?rik=FTuQfreqPmwLvw&pid=ImgRaw"
+    },
+    {
+      name:"gang beast",
+      url:"https://th.bing.com/th/id/R214821fa35fcfcd6d7aa99606e5d91a8?rik=sN3JZI0jgDt99g&pid=ImgRaw"
+    },
+    {
+      name:"clash of clans",
+      url:"https://th.bing.com/th/id/R4efb1952ba1e3f0788c5cdb814f3a714?rik=A8NIB9vCIgrmqw&riu=http%3a%2f%2fwww.goodluckhavefun.net%2fwp-content%2fuploads%2f2013%2f01%2fClash-of-Clans-iOS-Icon.png&ehk=fEXrDShBe%2b90rrdUvfV1KA55uVgREKOvW%2b4%2fNPejwjU%3d&risl=&pid=ImgRaw"
+    }
+  ]
+
+  for(let i = 0; i<games.length;i++){
+      console.log(games[i].name);
+  }
+
+
+// 5. array
+
+// crud => create, read, update, delete,
+
+// create
+// let fruit = ["cam", "táo", "kiwi","bưởi","đào","quất"]
+
+// //read
+
+// console.log(fruit);
+
+// //update
+
+// fruit.splice(3,1)
+// console.log(fruit);
+
+// 6 object
+
+// let cam = {
+//     name: "Qua Cam",
+//     color: "orange",
+//     taste: "sweet"
+// }
+// console.log(cam.name);
+
+
+
+
+
+
